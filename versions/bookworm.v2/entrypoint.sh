d2aa21f352617ee18880f4098c4db3023b53a670
Hagan Franks <224019+hagan@users.noreply.github.com>
2024-02-03 12:09:12 -0700

    giving up on merging images, using python+nodejs

--- versions/bookworm.v2/entrypoint.sh
+++ versions/bookworm.v2/entrypoint.sh
@@ -8,7 +8,7 @@ echo "/run/nginx/node-nextjs.socket?"
 # Take our environment variables from docker and insert into .env.local
 test -d /usr/local/src/awsmgr-ui && echo "NODE_SOCK=$NODE_SOCK" > /usr/local/src/awsmgr-ui/.env.local
 
-if [ ! -z "$RUNSHELL" && "$RUNSHELL" == "yes" ]; then
+if [ ! -z "$RUNSHELL" ] && [ "$RUNSHELL" == "yes" ]; then
   /usr/bin/bash
 else
   test -f /usr/bin/supervisord && /usr/bin/supervisord -c /etc/supervisord.conf || /usr/bin/bash

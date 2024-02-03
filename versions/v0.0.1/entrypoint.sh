3eada48d2e8304f032e4bac8067f548367fa1236
Hagan Franks <224019+hagan@users.noreply.github.com>
2024-02-03 10:23:19 -0700

    reorg folders

--- versions/v0.0.1/entrypoint.sh
+++ versions/v0.0.1/entrypoint.sh
@@ -5,6 +5,6 @@ echo "Welcome to AWS resource manager"
 echo "/run/nginx/node-nextjs.socket?"
 
 # Take our environment variables from docker and insert into .env.local
-echo "NODE_SOCK=$NODE_SOCK" > /usr/src/awsmgr-ui/.env.local
+test -d /usr/src/awsmgr-ui && echo "NODE_SOCK=$NODE_SOCK" > /usr/src/awsmgr-ui/.env.local
 
 /usr/bin/supervisord -c /etc/supervisord.conf

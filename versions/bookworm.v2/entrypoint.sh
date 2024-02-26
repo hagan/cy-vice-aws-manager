3635d80687299ddc81769317570eaa93582ef503
Hagan Franks <224019+hagan@users.noreply.github.com>
2024-02-25 19:17:07 -0700

    latest working version

--- versions/bookworm.v2/entrypoint.sh
+++ versions/bookworm.v2/entrypoint.sh
@@ -9,7 +9,15 @@ echo "/run/nginx/node-nextjs.socket?"
 #test -d /usr/local/src/awsmgr-ui && echo "NODE_SOCK=$NODE_SOCK" > /usr/local/src/awsmgr-ui/.env.local
 
 if [ ! -z "$RUNSHELL" ] && [ "$RUNSHELL" == "yes" ]; then
-  /usr/bin/bash
+  if [[ ! -z "$AWS_KMS_KEY" ]]; then
+    AWS_KMS_KEY=${AWS_KMS_KEY} /usr/bin/bash
+  else
+    /usr/bin/bash
+  fi
 else
-  test -f /usr/bin/supervisord && /usr/bin/supervisord -c /etc/supervisord.conf || /usr/bin/bash
+  if [[ ! -z "$AWS_KMS_KEY" ]]; then
+    test -f /usr/bin/supervisord && AWS_KMS_KEY=${AWS_KMS_KEY} /usr/bin/supervisord -c /etc/supervisord.conf || /usr/bin/bash
+  else
+    test -f /usr/bin/supervisord && /usr/bin/supervisord -c /etc/supervisord.conf || /usr/bin/bash
+  fi
 fi
--- versions/bookworm.v2/usr/local/bin/update-wheel.sh
+++ versions/bookworm.v2/usr/local/bin/update-wheel.sh
@@ -2,16 +2,16 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "$0")" && pwd)
 
-if [ $(whoami) != 'node' ]; then
-  echo "ERROR: must run awsmgr-start as 'node' user. Currently running as: $(whoami)"
-  exit 1
-fi
+# if [ $(whoami) != 'gunicorn' ]; then
+#   echo "ERROR: must run awsmgr-start as 'gunicorn' user. Currently running as: $(whoami)"
+#   exit 1
+# fi
 
 FLASK_VIRTUAL_ENV="$HOME/envs/flask-env/bin/activate"
 LATEST_WHL_APP=$(ls -lhtp /mnt/dist/wheels/*.whl | head -n1 | awk '{print $9}')
 
 if [ ! -f $LATEST_WHL_APP ]; then
-   echo "No packages in /mnt/dist/wheels to install!"
+   >&2 echo "ERROR: No packages in /mnt/dist/wheels to install!"
    exit 1
 elif [ -f $FLASK_VIRTUAL_ENV ]; then
   echo "Installing $LATEST_WHL_APP"
@@ -21,6 +21,6 @@ elif [ ! -z $VIRTUAL_ENV ]; then
   pip install  --force-reinstall --no-deps $LATEST_WHL_APP
   exit $?
 else
-  echo "ERROR: No virtualenv set or available to run in!"
+  >&2 echo "ERROR: No virtualenv set or available to run in!"
   exit 1
 fi

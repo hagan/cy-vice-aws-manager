1d3a059666aacf69e20a8ec9dd99987111be0d81
Hagan Franks <224019+hagan@users.noreply.github.com>
2024-02-27 14:00:55 -0700

    updated entrypoint, su can pass env vars!

--- versions/bookworm.v2/entrypoint.sh
+++ versions/bookworm.v2/entrypoint.sh
@@ -8,16 +8,9 @@ echo "/run/nginx/node-nextjs.socket?"
 # Take our environment variables from docker and insert into .env.local
 #test -d /usr/local/src/awsmgr-ui && echo "NODE_SOCK=$NODE_SOCK" > /usr/local/src/awsmgr-ui/.env.local
 
+## launch shell
 if [ ! -z "$RUNSHELL" ] && [ "$RUNSHELL" == "yes" ]; then
-  if [[ ! -z "$AWS_KMS_KEY" ]]; then
-    AWS_KMS_KEY=${AWS_KMS_KEY} /usr/bin/bash
-  else
-    /usr/bin/bash
-  fi
+  /usr/bin/bash
 else
-  if [[ ! -z "$AWS_KMS_KEY" ]]; then
-    test -f /usr/bin/supervisord && AWS_KMS_KEY=${AWS_KMS_KEY} /usr/bin/supervisord -c /etc/supervisord.conf || /usr/bin/bash
-  else
-    test -f /usr/bin/supervisord && /usr/bin/supervisord -c /etc/supervisord.conf || /usr/bin/bash
-  fi
+  /usr/bin/supervisord -c /etc/supervisord.conf || /usr/bin/bash
 fi

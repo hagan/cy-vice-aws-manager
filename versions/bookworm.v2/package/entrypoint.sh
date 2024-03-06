51e7debda42002861a6eca04b8c92f60729f74f9
Hagan Franks <224019+hagan@users.noreply.github.com>
2024-03-06 16:00:50 -0700

    disabled gunicorn/express, fixing acl and misc updates

--- versions/bookworm.v2/package/entrypoint.sh
+++ versions/bookworm.v2/package/entrypoint.sh
@@ -1,58 +1,81 @@
 #!/bin/sh
 set -e
 
+echo "DOCKER VERSION INFO"
+cat /etc/docker-image-ver
+
+# echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | /usr/bin/envsubst > /home/cyverse/.irods/irods_environment.json
+# chown cyverse:cyverse /home/cyverse/.irods/irods_environment.json
+# echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | /usr/bin/envsubst > /home/node/.irods/irods_environment.json
+# chown node:node /home/node/.irods/irods_environment.json
+# echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | /usr/bin/envsubst > /home/gunicorn/.irods/irods_environment.json
+# chown gunicorn:gunicorn /home/gunicorn/.irods/irods_environment.json
+
 if [ -z "${AWS_ACCESS_KEY_ID}" ]; then
-  echo -e "WARNING: 'AWS_ACCESS_KEY_ID' is unset!\n"
-  AWS_ACCESS_KEY_ID=
+  echo -e "'AWS_ACCESS_KEY_ID' is unset!\n"
 else
-  echo "Captured AWS_ACCESS_KEY_ID = '${AWS_ACCESS_KEY_ID}'"
+  : # noop
+  # echo "Captured AWS_ACCESS_KEY_ID = '${AWS_ACCESS_KEY_ID}'"
 fi
 
 if [ -z "${AWS_SECRET_ACCESS_KEY}" ]; then
-  echo -e "WARNING: 'AWS_SECRET_ACCESS_KEY' is unset!\n"
-  AWS_SECRET_ACCESS_KEY=
+  echo -e "'AWS_SECRET_ACCESS_KEY' environment variable is unset!\n"
 else
-  echo "Captured AWS_SECRET_ACCESS_KEY = '${AWS_SECRET_ACCESS_KEY//?/*}'"
+  : # noop
+  # echo "Captured AWS_SECRET_ACCESS_KEY = '${AWS_SECRET_ACCESS_KEY//?/*}'"
 fi
 
 if [ -z "${AWS_SESSION_TOKEN}" ]; then
-  AWS_SESSION_TOKEN=
+  echo -e "'AWS_SESSION_TOKEN' environment variable is unset!\n"
 else
-  echo "Captured AWS_SESSION_TOKEN = '${AWS_SESSION_TOKEN//?/*}'"
+  : # noop
+  # echo "Captured AWS_SESSION_TOKEN = '${AWS_SESSION_TOKEN//?/*}'"
 fi
 
-if [ -z "${AWS_KMS_KEY}" ]; then
-  AWS_KMS_KEY=
-else
-  echo "Captured AWS_KMS_KEY = '${AWS_KMS_KEY//?/*}'"
+if [ ! -z "${AWS_KMS_KEY}" ]; then
+  : # noop
+  # echo "Captured AWS_KMS_KEY = '${AWS_KMS_KEY//?/*}'"
 fi
 
 if [ -z "${AWS_DEFAULT_REGION}" ]; then
-  AWS_DEFAULT_REGION='us-west-2'
+  export AWS_DEFAULT_REGION='us-west-2'
 else
   echo "Captured AWS_DEFAULT_REGION = '${AWS_DEFAULT_REGION}'"
 fi
 
 if [ -z "${AWS_DEFAULT_PROFILE}" ]; then
-  AWS_DEFAULT_PROFILE=
+  export AWS_DEFAULT_PROFILE='default'
 else
-  echo "Captured AWS_DEFAULT_PROFILE = '${AWS_DEFAULT_PROFILE}'"
+  : # noop
+  # echo "Captured AWS_DEFAULT_PROFILE = '${AWS_DEFAULT_PROFILE}'"
 fi
 
-if [ -z ${EXPRESS_SOCKET_FILE} ]; then
+if [ ! -z ${EXPRESS_SOCKET_FILE} ]; then
   echo "Captured EXPRESS_SOCKET_FILE = '${EXPRESS_SOCKET_FILE}'"
-  exit 1
 fi
 
-if [ -z ${GUNICORN_SOCKET_FILE} ]; then
+if [ ! -z ${GUNICORN_SOCKET_FILE} ]; then
   echo "Captured GUNICORN_SOCKET_FILE = '${GUNICORN_SOCKET_FILE}'"
-  exit 1
 fi
 
-export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN AWS_KMS_KEY AWS_DEFAULT_REGION AWS_DEFAULT_PROFILE EXPRESS_SOCKET_FILE GUNICORN_SOCKET_FILE
+# export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN AWS_KMS_KEY AWS_DEFAULT_REGION 
 
 # Take our environment variables from docker and insert into .env.local
 #test -d /usr/local/src/awsmgr-ui && echo "NODE_SOCK=$NODE_SOCK" > /usr/local/src/awsmgr-ui/.env.local
+
+## WORKAROUND -> https://github.com/moby/moby/issues/40553 (this gets set in vice Dockerfile)
+setfacl \
+    -m u:cyverse:rwx,g:cyverse:rwx \
+    -m d:cyverse:rwx,g:cyverse:rwx \
+    -m o::r-x \
+    -m d:o::r-x /usr/local/var && { echo "-36-"; } || { echo "ERROR -36-"; exit 1; }
+setfacl \
+    -m u:cyverse:rwx,g:cyverse:rwx \
+    -m d:cyverse:rwx,g:cyverse:rwx \
+    -m o::--- \
+    -m d:o::--- /usr/local/var/pulumi && { echo "-37-"; } || { echo "ERROR -37-"; exit 1; }
+
+## END OF WORKAROUND
 
 ## launch shell
 if [ ! -z "$RUNSHELL" ] && [ "$RUNSHELL" == "yes" ]; then

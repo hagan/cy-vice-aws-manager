c64af8b6a3605970a6d827ef22628741ab8dec56
Hagan Franks <224019+hagan@users.noreply.github.com>
2024-04-04 21:24:56 -0700

    fixup

--- versions/bookworm.v3/package/usr/local/bin/poke-deadman.sh
+++ versions/bookworm.v3/package/usr/local/bin/poke-deadman.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-function get_cache_key() {
-    ## only works if pymemcache is available / install systemwide
-    python -c "from pymemcache.client.base import Client; print(str(Client(('localhost', 11212)).get('$1', b'').decode('utf-8')));"
+## dumb bug with how $1 is passed? hardcoded ver
+function get_apigateway_id_cache_key() {
+    python -c "from pymemcache.client.base import Client; print(str(Client(('localhost', 11212)).get('APIGATEWAY_ID', b'').decode('utf-8')));"
 }
 
-APIGATEWAY_ID=$(get_cache_key 'APIGATEWAY_ID')
-APIKEY_VALUE=$(get_cache_key 'APIKEY_VALUE')
+function get_apikey_value_cache_key() {
+    python -c "from pymemcache.client.base import Client; print(str(Client(('localhost', 11212)).get('APIKEY_VALUE', b'').decode('utf-8')));"
+}
+
+APIGATEWAY_ID=$(get_apigateway_id_cache_key)
+APIKEY_VALUE=$(get_apikey_value_cache_key)
 
 APIKEY_VALUE_LENGTH=${#APIKEY_VALUE}
 NUM_ASTERISKS=$((APIKEY_VALUE_LENGTH-4))
